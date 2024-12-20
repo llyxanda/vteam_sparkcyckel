e2326ba3de60e4a3e8f550d7d7c73a155a34c2b3
David A <david.allert@hotmail.com>
2024-12-20 22:36:34 +0100

    Added comment.

--- server/datamodels/user.mjs
+++ server/datamodels/user.mjs
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-    emmil: { type: String, required: true, unique: true },
+    emmil: { type: String, required: true, unique: true }, // FIX Can't see if this code is being used anywhere, but if it is then emmil -> email.
     password: { type: String, required: true },
 });
 
